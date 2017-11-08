59dc80bebbc5e5b810cc02d2dcfe5988c16a717f
dllo <15140211872@163.com>
2017-11-08 17:18:34 +0800

    Create mybatis config

---
+++ src/main/java/com/lanou/mapper/AreaMapper.java
@@ -0,0 +1,17 @@
+package com.lanou.mapper;
+
+import com.lanou.bean.Area;
+
+public interface AreaMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Area record);
+
+    int insertSelective(Area record);
+
+    Area selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Area record);
+
+    int updateByPrimaryKey(Area record);
+}
